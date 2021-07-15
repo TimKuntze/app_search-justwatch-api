c67b72bc7434459045cecf699879a4d5e5773c56
Tim <67963728+TimKuntze@users.noreply.github.com>
2021-07-15 20:59:57 +0200

    added justwatch api

--- src/components/myStore.vue
+++ src/components/myStore.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="hello">
     <header>
-        
       <h1>{{ msg }}</h1>
-      <img src="./../assets/film-reel-128_white.png">
+      <img src="./../assets/film-reel-128_white.png" />
     </header>
 
     <div class="search-and-results">
-      
       <div class="searcharea">
         <div class="input-container">
-            <img src="./../assets/search-2-128.png">
+          <img src="./../assets/search-2-128.png" />
           <input
+            @keypress="searchMovie"
             type="text"
             placeholder="Search for the movie here!"
           />
         </div>
       </div>
       <div class="results">
-          
-        <div class="result-item" v-for="gettersmovie in gettersmovies" :key="gettersmovie.id">
-          <div class="result-title"><b>Title:</b><br>{{ gettersmovie.name }}</div> <div class="result-link"><b>Link:</b><br>https://www.justwatch.com/{{ gettersmovie.website }}</div>
-          <div class="result-type"><b>Type:</b><br>{{ gettersmovie.company.name }}</div>
+        <div
+          class="result-item"
+          v-for="gettersmovie in gettersmovies"
+          :key="gettersmovie.items.id"
+        >
+          <div class="result-title">
+            <b>Title:</b><br />{{ gettersmovie.title }}
+          </div>
+          <div class="result-link">
+            <b>Link:</b><br />https://www.justwatch.com/{{
+              gettersmovie.full_path
+            }}
+          </div>
+          <div class="result-type">
+            <b>Type:</b><br />{{ gettersmovie.object_type }}
+          </div>
         </div>
       </div>
     </div>
@@ -30,6 +41,9 @@
 
 <script>
 export default {
+
+
+
   name: "myStore",
   data() {
     return {
@@ -44,6 +58,11 @@ export default {
   mounted() {
     this.$store.dispatch("getMovies");
   },
-
+  methods: {
+    searchMovie(e) {
+      this.$store.commit("searchMovie", e.target.value);
+      
+    },
+  },
 };
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -8,35 +8,57 @@ Vue.use(Vuex);
 
 //to handle state
 const state = {
-    movies: []
+  movies: [],
+  search: "",
+  body: {}
 }
 
 //to handle state
 const getters = {
-    allMovies: (state: { movies: any; }) => state.movies
+  allMovies: (state: { movies: any; }) => state.movies
+  
 }
 
 //to handle actions
 const actions = {
-    getMovies({ commit }:{ commit: any; }) {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(response => {
-                commit('SET_MOVIES', response.data)
-           })
-    }
+  getMovies({ commit }: { commit: any; }) {
+    axios.get('https://apis.justwatch.com/content/titles/en_US/popular?body=%7B%22fields%22:[%22cinema_release_date%22,%22full_path%22,%22full_paths%22,%22id%22,%22localized_release_date%22,%22object_type%22,%22poster%22,%22scoring%22,%22title%22,%22tmdb_popularity%22,%22backdrops%22,%22production_countries%22,%22offers%22,%22original_release_year%22,%22backdrops%22],%22enable_provider_filter%22:false,%22query%22:%22movie%22,%22monetization_types%22:[],%22page%22:3,%22page_size%22:20,%22matching_offers_only%22:false%7D&language=e')
+      .then(response => {
+        commit('SET_MOVIES', response.data);
+        console.log(response.data);
+      })
+  }
 }
 
 //to handle mutations
 const mutations = {
-    SET_MOVIES(state: { movies: any; }, movies: any) {
-        state.movies = movies
-    }
+  SET_MOVIES(state: { movies: any; }, movies: any) {
+    state.movies = movies
+    console.log('Movies:', movies)
+  },
+  searchMovie(state: any, search: string) {
+    state.search = search;
+    console.log(state.search);
+
+    state.body = { "fields": ["cinema_release_date", "full_path", "full_paths", "id", "localized_release_date", "object_type", "poster", "scoring", "title", "tmdb_popularity", "backdrops", "production_countries", "offers", "original_release_year", "backdrops"], "enable_provider_filter": false, "query": "", "monetization_types": [], "page": 1, "page_size": 20, "matching_offers_only": false }
+
+    axios.get(`https://apis.justwatch.com/content/titles/en_US/popular?body=%7B%22fields%22:[%22cinema_release_date%22,%22full_path%22,%22full_paths%22,%22id%22,%22localized_release_date%22,%22object_type%22,%22poster%22,%22scoring%22,%22${search}%22,%22tmdb_popularity%22,%22backdrops%22,%22production_countries%22,%22offers%22,%22original_release_year%22,%22backdrops%22],%22enable_provider_filter%22:false,%22query%22:%22movie%22,%22monetization_types%22:[],%22page%22:3,%22page_size%22:20,%22matching_offers_only%22:false%7D&language=e`)
+      .then(response => {
+        // commit('SET_MOVIES', response.data)
+        console.log('Searched movies', response.data)
+      })
+      
+  }
 }
 
 //export store module
 export default new Vuex.Store({
-    state,
-    getters,
-    actions,
-    mutations
+  state,
+  getters,
+  actions,
+  mutations
 })
+
+function allMovies(allMovies: any) {
+  throw new Error('Function not implemented.');
+}
